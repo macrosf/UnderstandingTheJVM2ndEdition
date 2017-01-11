ce8636ccec6d6c17e862a25bf12cd76d8df1aca7
macrosf@ocj <macros_f@163.com>
2017-01-11 19:04:42 +0800

    initialize

---
+++ UnderstandingTheJVM2ndEdition/src/main/java/understanding/the/jvm/second/edition/App.java
@@ -0,0 +1,13 @@
+package understanding.the.jvm.second.edition;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ UnderstandingTheJVM2ndEdition/src/main/java/understanding/the/jvm/second/edition/chapter02/codelist2_3/HeapOOM.java
@@ -0,0 +1,22 @@
+package understanding.the.jvm.second.edition.chapter02.codelist2_3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+*VM Args：-Xms20m-Xmx20m-XX：+HeapDumpOnOutOfMemoryError
+*@author zzm
+*/
+public class HeapOOM {
+
+	static class OOMObject{
+		
+	}
+	
+	public static void main(String[] args){
+		List<OOMObject> list = new ArrayList<OOMObject>();
+		while (true){
+			list.add(new OOMObject());
+		}
+	}
+}
