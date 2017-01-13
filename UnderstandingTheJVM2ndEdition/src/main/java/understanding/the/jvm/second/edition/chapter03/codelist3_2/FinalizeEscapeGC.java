13a5d6efdee6e82aa67f979d5382810decf2f589
macrosf@ocj <macros_f@163.com>
2017-01-13 11:18:33 +0800

    chapter03 codelist 3-2

---
+++ UnderstandingTheJVM2ndEdition/src/main/java/understanding/the/jvm/second/edition/chapter03/codelist3_2/FinalizeEscapeGC.java
@@ -0,0 +1,42 @@
+package understanding.the.jvm.second.edition.chapter03.codelist3_2;
+
+/**
+*此代码演示了两点：
+*1.对象可以在被GC时自我拯救。
+*2.这种自救的机会只有一次，因为一个对象的finalize（）方法最多只会被系统自动调用一次
+*@author zzm
+*/
+public class FinalizeEscapeGC {
+	public static FinalizeEscapeGC SAVE_HOOK=null;
+	public void isAlive(){
+		System.out.println("yes, i am still alive:)");
+	}
+	@Override
+	protected void finalize() throws Throwable{
+		super.finalize();
+		System.out.println("finalize method executed!");
+		SAVE_HOOK = this;
+	}
+	
+	public static void main(String[] args) throws Throwable{
+		SAVE_HOOK = new FinalizeEscapeGC();
+		SAVE_HOOK = null;
+		System.gc();
+		Thread.sleep(500);
+		if (SAVE_HOOK!=null){
+			SAVE_HOOK.isAlive();
+		}else {
+			System.out.println("no, i am dead :(");
+		}
+		
+		SAVE_HOOK = null;
+		System.gc();
+		Thread.sleep(500);
+		if (SAVE_HOOK!=null){
+			SAVE_HOOK.isAlive();
+		}else {
+			System.out.println("no, i am dead :(");
+		}
+		
+	}
+}
