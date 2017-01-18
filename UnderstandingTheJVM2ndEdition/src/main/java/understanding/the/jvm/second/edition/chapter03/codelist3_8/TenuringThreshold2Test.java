eb842d3d57a58ec6187e382d208f1e48cd507c34
macrosf@ocj <macros_f@163.com>
2017-01-18 17:25:21 +0800

    end of chapter03

--- UnderstandingTheJVM2ndEdition/src/main/java/understanding/the/jvm/second/edition/chapter03/codelist3_8/TenuringThreshold2Test.java
+++ UnderstandingTheJVM2ndEdition/src/main/java/understanding/the/jvm/second/edition/chapter03/codelist3_8/TenuringThreshold2Test.java
@@ -22,22 +22,42 @@ public class TenuringThreshold2Test {
 }
 
 /*
-[GC[DefNew
+[GC [DefNew
 Desired survivor size 524288 bytes, new threshold 1 (max 15)
-- age   1:    1012176 bytes,    1012176 total
-: 5443K->988K(9216K), 0.0030681 secs] 5443K->5084K(19456K), 0.0031038 secs] [Times: user=0.00 sys=0.00, real=0.00 secs] 
-[GC[DefNew
+- age   1:     682128 bytes,     682128 total
+: 5115K->666K(9216K), 0.0026774 secs] 5115K->4762K(19456K), 0.0026977 secs] [Times: user=0.02 sys=0.00, real=0.00 secs] 
+[GC [DefNew
 Desired survivor size 524288 bytes, new threshold 15 (max 15)
-- age   1:        136 bytes,        136 total
-: 5248K->0K(9216K), 0.0014303 secs] 9344K->5083K(19456K), 0.0015516 secs] [Times: user=0.00 sys=0.00, real=0.00 secs] 
+: 4762K->0K(9216K), 0.0006649 secs] 8858K->4762K(19456K), 0.0006799 secs] [Times: user=0.00 sys=0.00, real=0.00 secs] 
 Heap
- def new generation   total 9216K, used 4260K [0x00000000f9a00000, 0x00000000fa400000, 0x00000000fa400000)
-  eden space 8192K,  52% used [0x00000000f9a00000, 0x00000000f9e28fd0, 0x00000000fa200000)
-  from space 1024K,   0% used [0x00000000fa200000, 0x00000000fa200088, 0x00000000fa300000)
-  to   space 1024K,   0% used [0x00000000fa300000, 0x00000000fa300000, 0x00000000fa400000)
- tenured generation   total 10240K, used 5083K [0x00000000fa400000, 0x00000000fae00000, 0x00000000fae00000)
-   the space 10240K,  49% used [0x00000000fa400000, 0x00000000fa8f6e98, 0x00000000fa8f7000, 0x00000000fae00000)
- compacting perm gen  total 21248K, used 2616K [0x00000000fae00000, 0x00000000fc2c0000, 0x0000000100000000)
-   the space 21248K,  12% used [0x00000000fae00000, 0x00000000fb08e3a8, 0x00000000fb08e400, 0x00000000fc2c0000)
+ def new generation   total 9216K, used 4178K [0x04670000, 0x05070000, 0x05070000)
+  eden space 8192K,  51% used [0x04670000, 0x04a84828, 0x04e70000)
+  from space 1024K,   0% used [0x04e70000, 0x04e70000, 0x04f70000)
+  to   space 1024K,   0% used [0x04f70000, 0x04f70000, 0x05070000)
+ tenured generation   total 10240K, used 4762K [0x05070000, 0x05a70000, 0x05a70000)
+   the space 10240K,  46% used [0x05070000, 0x055168a0, 0x05516a00, 0x05a70000)
+ compacting perm gen  total 12288K, used 2123K [0x05a70000, 0x06670000, 0x09a70000)
+   the space 12288K,  17% used [0x05a70000, 0x05c82e20, 0x05c83000, 0x06670000)
 No shared spaces configured.
+
+****注释掉--> allocation2 = new byte[_1MB/4]; <--这句话后后的执行结果（注意第二次minor GC后DefNew里面剩余了409K数据：“4506K->409K(9216K),”） 注：jre=1.6
+[GC [DefNew
+Desired survivor size 524288 bytes, new threshold 15 (max 15)
+- age   1:     419968 bytes,     419968 total
+: 4859K->410K(9216K), 0.0024859 secs] 4859K->4506K(19456K), 0.0025030 secs] [Times: user=0.00 sys=0.00, real=0.00 secs] 
+[GC [DefNew
+Desired survivor size 524288 bytes, new threshold 15 (max 15)
+- age   2:     419824 bytes,     419824 total
+: 4506K->409K(9216K), 0.0004500 secs] 8602K->4506K(19456K), 0.0004650 secs] [Times: user=0.00 sys=0.00, real=0.00 secs] 
+Heap
+ def new generation   total 9216K, used 4588K [0x046c0000, 0x050c0000, 0x050c0000)
+  eden space 8192K,  51% used [0x046c0000, 0x04ad4828, 0x04ec0000)
+  from space 1024K,  40% used [0x04ec0000, 0x04f267f0, 0x04fc0000)
+  to   space 1024K,   0% used [0x04fc0000, 0x04fc0000, 0x050c0000)
+ tenured generation   total 10240K, used 4096K [0x050c0000, 0x05ac0000, 0x05ac0000)
+   the space 10240K,  40% used [0x050c0000, 0x054c0010, 0x054c0200, 0x05ac0000)
+ compacting perm gen  total 12288K, used 2123K [0x05ac0000, 0x066c0000, 0x09ac0000)
+   the space 12288K,  17% used [0x05ac0000, 0x05cd2df8, 0x05cd2e00, 0x066c0000)
+No shared spaces configured.
+
  */
--- UnderstandingTheJVM2ndEdition/src/main/java/understanding/the/jvm/second/edition/chapter03/codelist3_9/HandlePromotionTest.java
+++ UnderstandingTheJVM2ndEdition/src/main/java/understanding/the/jvm/second/edition/chapter03/codelist3_9/HandlePromotionTest.java
@@ -23,3 +23,41 @@ public class HandlePromotionTest {
 		allocation7 = new byte[2*_1MB];
 	}
 }
+/*
+****以HandlePromotionFailure=false参数来运行的结果：****(一次minor GC，一次full GC)
+[GC [DefNew
+Desired survivor size 524288 bytes, new threshold 15 (max 15)
+- age   1:     157784 bytes,     157784 total
+: 6651K->154K(9216K), 0.0025400 secs] 6651K->4250K(19456K), 0.0025633 secs] [Times: user=0.00 sys=0.00, real=0.00 secs] 
+[GC [DefNew: 6384K->6384K(9216K), 0.0000075 secs][Tenured: 4096K->4250K(10240K), 0.0030622 secs] 10480K->4250K(19456K), [Perm : 2114K->2114K(12288K)], 0.0031013 secs] [Times: user=0.00 sys=0.00, real=0.00 secs] 
+Heap
+ def new generation   total 9216K, used 2211K [0x04620000, 0x05020000, 0x05020000)
+  eden space 8192K,  27% used [0x04620000, 0x04848fd8, 0x04e20000)
+  from space 1024K,   0% used [0x04f20000, 0x04f20000, 0x05020000)
+  to   space 1024K,   0% used [0x04e20000, 0x04e20000, 0x04f20000)
+ tenured generation   total 10240K, used 4250K [0x05020000, 0x05a20000, 0x05a20000)
+   the space 10240K,  41% used [0x05020000, 0x05446878, 0x05446a00, 0x05a20000)
+ compacting perm gen  total 12288K, used 2123K [0x05a20000, 0x06620000, 0x09a20000)
+   the space 12288K,  17% used [0x05a20000, 0x05c32ea8, 0x05c33000, 0x06620000)
+No shared spaces configured.
+
+****以HandlePromotionFailure=true参数来运行的结果：***(两次full GC)
+[GC [DefNew
+Desired survivor size 524288 bytes, new threshold 15 (max 15)
+- age   1:     157784 bytes,     157784 total
+: 6651K->154K(9216K), 0.0023549 secs] 6651K->4250K(19456K), 0.0023717 secs] [Times: user=0.00 sys=0.00, real=0.00 secs] 
+[GC [DefNew
+Desired survivor size 524288 bytes, new threshold 15 (max 15)
+- age   2:     157784 bytes,     157784 total
+: 6384K->154K(9216K), 0.0003586 secs] 10480K->4250K(19456K), 0.0003813 secs] [Times: user=0.00 sys=0.00, real=0.00 secs] 
+Heap
+ def new generation   total 9216K, used 2366K [0x04760000, 0x05160000, 0x05160000)
+  eden space 8192K,  27% used [0x04760000, 0x04988fd8, 0x04f60000)
+  from space 1024K,  15% used [0x04f60000, 0x04f86858, 0x05060000)
+  to   space 1024K,   0% used [0x05060000, 0x05060000, 0x05160000)
+ tenured generation   total 10240K, used 4096K [0x05160000, 0x05b60000, 0x05b60000)
+   the space 10240K,  40% used [0x05160000, 0x05560020, 0x05560200, 0x05b60000)
+ compacting perm gen  total 12288K, used 2123K [0x05b60000, 0x06760000, 0x09b60000)
+   the space 12288K,  17% used [0x05b60000, 0x05d72ea8, 0x05d73000, 0x06760000)
+No shared spaces configured.
+ */ 
